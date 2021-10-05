440d9898209355de00ca038d35416cde5ecc8696
Maria Mendez <70541977+merimendez147@users.noreply.github.com>
2021-10-05 15:50:59 -0300

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,19 @@
+package CentroHemoterapiav2;
+
+
+
+/**
+ *
+ * @author elina
+ */
+public class Persona {
+
+    String nombre;
+    Clinica clinica;
+
+    public Persona(String n, Clinica c) {
+        this.nombre = n;
+        this.clinica = c;
+    }
+
+}
